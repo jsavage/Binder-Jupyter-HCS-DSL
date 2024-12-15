f89002f9949a32044e5d41e21669a8175fa744d1
JS <consult.js@gmail.com>
2024-12-15 14:44:59 +0000

    Clean repository structure for Binder

--- fix1.ps1
+++
@@ -1,4 +0,0 @@
-$iconPath = "C:\Users\consu\OneDrive\Documents\HCS_from_pi\jupyter-DSL\_output\static\favicons"
-takeown /F $iconPath /R
-icacls $iconPath /grant administrators:F /T
-Remove-Item $iconPath -Force -Recurse
